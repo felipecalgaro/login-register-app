fbde02609e62be2c7c4690360be3d59f7b504cb1
Felipe <pipecalgaro@gmail.com>
2022-08-17 16:58:25 -0300

    add new migration: name not unique anymore

--- server/useCases/CheckUserCredentialsUseCase.ts
+++ server/useCases/CheckUserCredentialsUseCase.ts
@@ -4,22 +4,22 @@ import { CheckUserProps } from "../../src/types/User";
 import { User } from "../entities/User";
 
 export class CheckUserCredentialsUseCase extends User {
-    constructor({ name, email, password }: CheckUserProps) {
-        super(name, email, password)
-    }
+	constructor({ name, email, password }: CheckUserProps) {
+		super(name, email, password)
+	}
 
-    async check(): ReturnedUserFromDatabase {
-        try {
-            const prisma = new PrismaClient()
-            const user = await prisma.user.findFirst({
-                where: {
-                    AND: [{ name: this.name }, { password: this.password }]
-                }
-            })
-            return user
-        } catch (err) {
-            console.log(err);
-            return null
-        }
-    }
+	async check(): ReturnedUserFromDatabase {
+		try {
+			const prisma = new PrismaClient()
+			const user = await prisma.user.findFirst({
+				where: {
+					AND: [{ name: this.name }, { password: this.password }]
+				}
+			})
+			return user
+		} catch (err) {
+			console.log(err);
+			return null
+		}
+	}
 }
